373cc43ab06639a572ad472d6cd14865c003e151
vives <vsvivevives@gmail.com>
2016-07-07 15:20:23 +0530

    Update YelpConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/yelp/YelpConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/yelp/YelpConnectorIntegrationTest.java
@@ -46,7 +46,7 @@ public class YelpConnectorIntegrationTest extends ConnectorIntegrationTestBase {
      */
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
-        init("yelp-connector-1.0.1");
+        init("yelp-connector-1.0.2-SNAPSHOT");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
     }
